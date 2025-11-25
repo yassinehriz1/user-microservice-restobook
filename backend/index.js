41b342baf5da3a56f36a81014f74fa976664dd54
yassinehriz1 <yassinehriz532@gmail.com>
2025-11-25 22:04:56 +0100

    test metrics

--- backend/index.js
+++ backend/index.js
@@ -32,8 +32,7 @@ collectDefaultMetrics();
 
 
 app.get('/metrics', async (req, res) => {
-  res.set('Content-Type', client.register.contentType);
-  res.end(await client.register.metrics());
+  res.send("hello")
 });
 
 app.use('/api/auth',registerRoute)
